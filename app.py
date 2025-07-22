7cfd912da023a9f058db066d2521d17f98b55303
Ananya Sahu <ananya1sahu@gmail.com>
2025-07-22 11:10:24 -0400

    updates

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ ADMIN_SECRET = "my_super_secret_key"# change this to something unique
 
 def is_admin():
     try:
-        return st.query_params.get("secret")[0] == ADMIN_SECRET
+        return st.query_params.get("secret") == ADMIN_SECRET
     except Exception:
         return False
 
